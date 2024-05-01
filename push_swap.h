dd86c29fbe87265d20f8c4d6b5f9e6809f4f6202
Sanda Hajaina Ranaivoarisoa <sranaivo@c1r7s5.42antananarivo.mg>
2024-05-01 16:33:38 +0300

    first init

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sranaivo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 10:19:13 by sranaivo          #+#    #+#             */
+/*   Updated: 2024/05/01 10:20:01 by sranaivo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+#endif
